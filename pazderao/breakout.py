326dd0f6f5ebc4e4e8da47626f2483908eb50a19
pazderao <paz.ondrej@gmail.com>
2026-03-11 10:08:21 +0100

    save

--- pazderao/breakout.py
+++ pazderao/breakout.py
@@ -1,38 +1,22 @@
 import pygame
 from pygame.locals import *
 import random
+from settings import *
 
 pygame.init()
 
-screen_width = 600
-screen_height = 600
-
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Breakout')
 
-#hlavní grafika
-bg = pygame.image.load("Projekt-main/images/background.jpg").convert()
-background = pygame.transform.scale(bg, (screen_width, screen_height))
-block_yellow = (223, 188, 9)
-block_orange = (246, 137, 27)
-block_red = (250, 85, 35)
-outline_col = (0, 0, 0)
-paddle_col = (0, 204, 102)
-paddle_outline = (100, 100, 100)
-#barvy textů a fonty
+#background
+BG = pygame.image.load(BG_IMAGE_PATH).convert()
+background = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+#font 
 font = pygame.font.Font("Projekt-main/images/font/lemonmilk.otf", 27)
 font1 = pygame.font.Font("Projekt-main/images/font/lemonmilk.otf", 50)
-text_col = (255, 255, 255)
-text_col_red = (255, 0, 0)
-text_col_green = (0, 255, 0)
 
-#základní proměnné pro hru
-cols = 6
-rows = 7
-clock = pygame.time.Clock()
-fps = 60
-live_ball = False
-game_over = 0
+
 
 #menu obtížností
 in_menu = True
@@ -48,7 +32,7 @@ def draw_text(text, font, text_col, x, y):
 #stěna z bloků
 class wall():
     def __init__(self):
-        self.width = screen_width // cols
+        self.width = SCREEN_WIDTH // COLS
         self.height = 50
 
     def clear_wall(self):  #odebrání bloků při konci hry
@@ -59,7 +43,7 @@ class wall():
         
         
         #Výběr úrovně a nastavení bloků
-        current_rows = rows
+        current_rows = ROWS
         if selected_level == 1: #easy
             current_rows = 5 # 
         elif selected_level == 3: #hard
@@ -71,7 +55,7 @@ class wall():
         block_individual = []
         for row in range(current_rows):
             block_row = []
-            for col in range(cols):
+            for col in range(COLS):
                 block_x = col * self.width
                 block_y = row * self.height
                 rect = pygame.Rect(block_x, block_y, self.width, self.height)
@@ -106,13 +90,13 @@ class wall():
             for block in row:
                 #barva bloku podle síly
                 if block[1] == 3:
-                    block_col = block_red
+                    block_col = BLOCK_RED
                 elif block[1] == 2:
-                    block_col = block_orange
+                    block_col = BLOCK_ORANGE
                 elif block[1] == 1:
-                    block_col = block_yellow
+                    block_col = BLOCK_YELLOW
                 pygame.draw.rect(screen, block_col, block[0])
-                pygame.draw.rect(screen, outline_col, (block[0]), 2)
+                pygame.draw.rect(screen, OUTLINE_COL, (block[0]), 2)
 
 
 
@@ -128,13 +112,13 @@ class paddle():
         if key[pygame.K_LEFT] and self.rect.left > 0:
             self.rect.x -= self.speed
             self.direction = -1
-        if key[pygame.K_RIGHT] and self.rect.right < screen_width:
+        if key[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
             self.rect.x += self.speed
             self.direction = 1
 #vykreslení pálky
     def draw(self):
-        pygame.draw.rect(screen, paddle_col, self.rect)
-        pygame.draw.rect(screen, paddle_outline, self.rect, 3)
+        pygame.draw.rect(screen, PADDLE_COL, self.rect)
+        pygame.draw.rect(screen, PADDLE_OUTLINE, self.rect, 3)
 
 
     def reset(self):
@@ -142,7 +126,7 @@ class paddle():
         self.height = 15
         
         # Změna velikosti pálky podle obtížnosti
-        base_width = int(90 * screen_width // 600) 
+        base_width = int(90 * SCREEN_WIDTH // 600) 
         
         if selected_level == 1: 
             self.width = base_width + 30
@@ -151,8 +135,8 @@ class paddle():
         else: 
             self.width = base_width
 
-        self.x = int((screen_width / 2) - (self.width / 2))
-        self.y = screen_height - (self.height * 2)
+        self.x = int((SCREEN_WIDTH / 2) - (self.width / 2))
+        self.y = SCREEN_HEIGHT - (self.height * 2)
         self.speed = 5 
         self.rect = Rect(self.x, self.y, self.width, self.height)
         self.direction = 0
@@ -196,19 +180,19 @@ class game_ball():
                 item_count += 1
             row_count += 1
         if wall_destroyed == 1:
-            self.game_over = 1
+            self.GAME_OVER = 1
 
 
 
         #kolize se stěnami
-        if self.rect.left < 0 or self.rect.right > screen_width:
+        if self.rect.left < 0 or self.rect.right > SCREEN_WIDTH:
             self.speed_x *= -1
 
         #kolize s horní a dolní hranou
         if self.rect.top < 0:
             self.speed_y *= -1
-        if self.rect.bottom > screen_height:
-            self.game_over = -1
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.GAME_OVER = -1
 
 
         #kolize s pálkou
@@ -229,12 +213,12 @@ class game_ball():
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
 
-        return self.game_over
+        return self.GAME_OVER
 
 
     def draw(self):
-        pygame.draw.circle(screen, paddle_col, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad), self.ball_rad)
-        pygame.draw.circle(screen, paddle_outline, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad), self.ball_rad, 3)
+        pygame.draw.circle(screen, PADDLE_COL, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad), self.ball_rad)
+        pygame.draw.circle(screen, PADDLE_OUTLINE, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad), self.ball_rad, 3)
 
 
 
@@ -252,7 +236,7 @@ class game_ball():
             self.speed_y = -4 
             
         self.speed_max = 5
-        self.game_over = 0
+        self.GAME_OVER = 0
 
 
 
@@ -265,16 +249,16 @@ ball = game_ball(player_paddle.x + (player_paddle.width // 2), player_paddle.y -
 run = True
 while run:
 
-    clock.tick(fps)
+    clock.tick(FPS)
     
-    screen.blit(bg, (0, 0))
+    screen.blit(BG, (0, 0))
 
     #Menu obtížností
     if in_menu:
         draw_text('VYBER OBTÍŽNOST:', font1, text_col, 45, 100)
-        draw_text('1 - LEHKÁ (EASY)', font, block_yellow, 180, 250)
-        draw_text('2 - STŘEDNÍ (MEDIUM)', font, block_orange, 150, 300)
-        draw_text('3 - TĚŽKÁ (HARD)', font, block_red, 180, 350)
+        draw_text('1 - LEHKÁ (EASY)', font, BLOCK_YELLOW, 180, 250)
+        draw_text('2 - STŘEDNÍ (MEDIUM)', font, BLOCK_ORANGE, 150, 300)
+        draw_text('3 - TĚŽKÁ (HARD)', font, BLOCK_RED, 180, 350)
         
         key = pygame.key.get_pressed()
         start_game = False
@@ -302,19 +286,19 @@ while run:
         player_paddle.draw()
         ball.draw()
 
-        if live_ball:
+        if LIVE_BALL:
             player_paddle.move()
-            game_over = ball.move()
-            if game_over != 0:
-                live_ball = False
+            GAME_OVER = ball.move()
+            if GAME_OVER != 0:
+                LIVE_BALL = False
 
-        if not live_ball:
-            if game_over == 0:
+        if not LIVE_BALL:
+            if GAME_OVER == 0:
                 draw_text('KLIKNI PRO START', font, text_col, 160, 430)
-            elif game_over == 1:
+            elif GAME_OVER == 1:
                 draw_text('VYHRÁL SI!', font1, text_col_green, 130, 180)
                 draw_text('KLIKNI PRO MENU', font, text_col, 165, 300)
-            elif game_over == -1:
+            elif GAME_OVER == -1:
                 wall.clear_wall()
                 draw_text('PROHRÁL SI!', font1, text_col_red, 120, 180)
                 draw_text('KLIKNI PRO MENU', font, text_col, 165, 300)
@@ -326,12 +310,12 @@ while run:
         # Ovládání hry
         if event.type == pygame.MOUSEBUTTONDOWN or (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
             if not in_menu:
-                if live_ball == False:
-                    if game_over != 0:
+                if LIVE_BALL == False:
+                    if GAME_OVER != 0:
                         in_menu = True
-                        game_over = 0
+                        GAME_OVER = 0
                     else:
-                        live_ball = True
+                        LIVE_BALL = True
                         ball.reset(player_paddle.x + (player_paddle.width // 2), player_paddle.y - player_paddle.height)
 
     pygame.display.update()
---
+++ pazderao/settings.py
@@ -0,0 +1,38 @@
+import pygame
+
+# Rozměry okna
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+# Cesty k souborům
+BG_IMAGE_PATH = "Projekt-main/images/background.jpg"
+FONT_PATH = "Projekt-main/images/font/lemonmilk.otf"
+
+# Základní nastavení hry
+COLS = 6
+ROWS = 7
+clock = pygame.time.Clock()
+FPS = 60
+LIVE_BALL = False
+GAME_OVER = 0
+
+# Grafika
+text_col = (255, 255, 255)
+text_col_red = (255, 0, 0)
+text_col_green = (0, 255, 0)
+
+# Barvy bloků
+BLOCK_YELLOW = (223, 188, 9)
+BLOCK_ORANGE = (246, 137, 27)
+BLOCK_RED = (250, 85, 35)
+
+# Ostatní barvy
+OUTLINE_COL = (0, 0, 0)
+PADDLE_COL = (0, 204, 102)
+PADDLE_OUTLINE = (100, 100, 100)
+
+# Barvy textů
+TEXT_COL = (255, 255, 255)
+TEXT_COL_RED = (255, 0, 0)
+TEXT_COL_GREEN = (0, 255, 0)
+
